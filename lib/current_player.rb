34d9eadb95700364705b14e20f1cc15e12e7507b
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-19 12:30:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,16 +3,6 @@ def turn_count(board)
     board.each do |value|
     if value !="" || value != " "
     count +=1    
-  end
 end
   return count
-
-end
-
-def current_player(turn)
-  if turn % 2 == 0 
-    return 'X'
-  else 
-    return 'O'
-  end
 end
